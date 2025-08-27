2df71224bfa0cb786c73ad0b4bf7ea9c72aacf70
Bashar Alwarad <beelwarad52@gmail.com>
2025-08-27 14:55:17 +0200

    method overloading corrections with namespace

--- 22_method_overloading/Program.cs
+++ 22_method_overloading/Program.cs
@@ -2,41 +2,26 @@
 using System;
 
 // Method overloading examples
-void Print(string message)
-{
-    Console.WriteLine(message);
-}
 
-void Print(string message, int times)
+namespace MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Print("Hi", 3);
+            Print(42);
+        }
+static void Print(string message, int times)
 {
     for (int i = 0; i < times; i++)
         Console.WriteLine(message);
 }
 
-void Print(int number)
+static void Print(int number)
 {
     Console.WriteLine($"Number: {number}");
 }
 
-// Calling overloaded methods
-Print("Hello");
-Print("Hi", 3);
-Print(42);
-
-// Overloading with different parameter order
-void Show(string text, double value)
-{
-    Console.WriteLine($"Text: {text}, Value: {value}");
+    }
 }
-
-void Show(double value, string text)
-{
-    Console.WriteLine($"Value: {value}, Text: {text}");
-}
-
-Show("Result", 3.14);
-Show(2.71, "Pi");
-
-// Note: Overloading by return type only is not allowed
-// int Add(int x, int y) { return x + y; }
-// double Add(int x, int y) { return x + y; } // Error: same parameter types
