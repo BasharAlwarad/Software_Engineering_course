c23ff022e57ebc8bdba0978b710c4355275c0d03
Bashar Alwarad <beelwarad52@gmail.com>
2025-11-02 09:14:15 +0100

    review

--- Exceptions/obj/Debug/net9.0/Exceptions.AssemblyInfo.cs
+++ Exceptions/obj/Debug/net9.0/Exceptions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exceptions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6693fd77694c90579525af09baeb1cc28110433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+480947a5a6ce763e393c05bb345ad6cd718a4ced")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exceptions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exceptions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
