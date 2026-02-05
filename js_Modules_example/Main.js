4aba5cec09e3de28fbb6e3e4655d3db55fd9f81c
Bashar Alwarad <beelwarad52@gmail.com>
2026-02-05 08:54:32 +0100

    adding js_moduls_example

--- js_Modules_example/Main.js
+++ js_Modules_example/Main.js
@@ -2,12 +2,9 @@ import App from './app.js';
 
 const root = document.getElementById('root');
 
-const x = async () => {
+const render = async () => {
   const html = await App();
   root.innerHTML = html;
 };
 
-x();
-// App().then((html) => {
-//   root.innerHTML = html;
-// });
+render();
