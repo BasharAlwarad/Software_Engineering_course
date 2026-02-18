68b037c8a93fc4c9bd5869c6f97b28458aaa5438
Bashar Alwarad <beelwarad52@gmail.com>
2026-02-18 08:19:06 +0100

    adding lecture react vite tailwind and daisyui

---
+++ React_vite_tailwind_daisyUi/src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from './components/Nav';
+import FormInput from './components/FormInput';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <FormInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ React_vite_tailwind_daisyUi/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className="w-full bg-blue-500">
+      <ul className="flex gap-4 justify-center items-center">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
