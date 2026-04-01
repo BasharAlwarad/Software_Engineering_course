36e06300193083ad5fc0663050d353ffc689e48d
Bashar Alwarad <beelwarad52@gmail.com>
2026-04-01 08:11:31 +0200

    ts with node

---
+++ my-ts-app/src/pages/index.ts
@@ -0,0 +1,6 @@
+// import { about } from './about';
+// import { contact } from './contact';
+
+// export { about, contact };
+export { about } from './about';
+export { contact } from './contact';
---
+++ my-ts-app/src/utils/index.ts
@@ -0,0 +1,12 @@
+// src/utils/index.ts
+/**
+ * Returns a random element from an array.
+ */
+export function pickRandom<T>(arr: T[]): T | undefined {
+  if (arr.length === 0) return undefined;
+  const index: number = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
+import {hello} from './x'
+export {hello}
